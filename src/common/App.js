39bab9b4345962a8e04f44e7b0f131a49c0b5875
Matthew Wang <matt8p@outlook.com>
2020-11-21 02:03:55 -0700

    learn react!

---
+++ src/common/App.js
@@ -0,0 +1,13 @@
+import '../common/App.css';
+import Navbar from '../components/navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <p>piss on yourself</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import './navbar.css'
+
+function Navbar()
+{
+    return (
+        <nav>Navbar</nav>
+    )
+}
+
+export default Navbar
