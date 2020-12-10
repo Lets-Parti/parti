dafd63e58bd8445afae73f05e88e4e50cb11aee5
aarichan <aarichan888@gmail.com>
2020-12-10 16:14:47 -0500

    yes

--- src/pages/user.js
+++ src/pages/user.js
@@ -4,8 +4,9 @@ import React from 'react';
 import '../stylesheets/common.css'
 import '../stylesheets/user.css'
 import profileImage from '../resources/images/no_img.jpg'
+import testImage from './test-imgs/test_img.jpg'
 
-//MaterialUI Imports 
+//MaterialUI Imports
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -22,22 +23,22 @@ import PropTypes from 'prop-types'
 class User extends React.Component
 {
     constructor()
-    {   
+    {
         super()
         this.state = {
-            
+
         }
-    }   
+    }
 
     // componentDidMount()
     // {
     //     const handle = this.props.match.params.userhandle
-    //     this.props.getUserByHandle(handle); 
+    //     this.props.getUserByHandle(handle);
     // }
 
     render()
-    {   
-        // const {user, isLoading} = this.props.data; 
+    {
+        // const {user, isLoading} = this.props.data;
         // let userDisplay = null
 
         // if(user && !isLoading)
@@ -50,7 +51,7 @@ class User extends React.Component
 
         return(
             <div className="userContainer">
-                    <Grid container>
+                    <Grid container spacing={3}>
                         {/* First Row */}
                         <Grid item className="grid-item-userinfo" sm={2} xs={4} align="center">
                             <img className="profile-image" src={profileImage} />
@@ -71,38 +72,55 @@ class User extends React.Component
 
                         {/* Second Row */}
                         <Grid item className="grid-item-bigpicture" sm={6} xs={12}>
-                            
+                            <img className="bigpicture-section" src={testImage}></img>
                         </Grid>
-                        <Grid item className="grid-item-bigpicture" sm={6} xs={12}>
-                            
-                        </Grid>
+                        <Grid container className="grid-item-smallpictures" sm={6} xs={12}>
+                            <Grid item sm={6} xs={6}>
+                              <img className="smallpicture-section" src={testImage}></img>
+                            </Grid>
+                            <Grid item sm={6} xs={6}>
+                              <img className="smallpicture-section" src={testImage}></img>
+                            </Grid>
+                            <Grid item sm={6} xs={6}>
+                              <img className="smallpicture-section" src={testImage}></img>
+                            </Grid>
+                            <Grid item sm={6} xs={6}>
+                              <img className="smallpicture-section" src={testImage}></img>
+                            </Grid>
+                          </Grid>
+
 
                         {/* Third Row */}
                         <Grid item className="grid-item-description" sm={6} xs={12}>
-                            
+
                         </Grid>
                         <Grid item className="grid-item-request" sm={6} xs={12}>
-                            
+
                         </Grid>
 
                         {/* Fourth Row */}
                         <Grid item className="grid-item-reviews" sm={12} xs={12}>
-                            
+                          <div className="reviews-section">
+                            <h1 className="banner">Review</h1>
+                            <hr></hr>
+                            <p>This is placeholder for a review</p>
+                            <p>This is second review</p>
+                          </div>
                         </Grid>
                     </Grid>
             </div>
-                
+
         )
     }
 }
 
 User.propTypes = {
-    getUserByHandle: PropTypes.func.isRequired, 
-    user: PropTypes.object.isRequired, 
+    getUserByHandle: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
 }
 
 const mapStateToProps = (state) => ({
-    data: state.data, 
+    data: state.data,
 })
 
 const mapActionsToProps = {
