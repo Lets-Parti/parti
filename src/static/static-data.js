f7abe19ea48b3b8a7487090e81300c280672837f
anish808 <lganish@gmail.com>
2021-01-01 19:54:56 -0700

    m

--- src/static/static-data.js
+++ src/static/static-data.js
@@ -1,43 +1,34 @@
 const StaticData = 
 {
     options: [
-        { value: 'DJ', label: 'DJ' },
-        { value: 'Photography', label: 'Photography'},
-        { value: 'Photo Booth', label: `Photo Booth`},
-        { value: 'Decorator', label: `Decorator`},
-        { value: 'Food Catering', label: `Food Catering`},
-        { value: 'Bounce House', label: `Bounce House`},
-        { value: 'AV', label: `Audio Visual Equipment (AV)`},
-        { value: 'Food Truck', label: `Food Truck`},
-        { value: 'Casino Rental', label: `Casino`},
-        { value: 'Petting Zoo', label: `Petting Zoo`},
-        { value: 'Clown', label: `Clown`},
-        { value: 'Magician', label: `Magician`},
-        { value: 'Comedian', label: `Comedian`},
-        { value: 'Circus', label: `Circus/Acrobat`},
-        { value: 'Live Band', label: `Live Band`},
-        { value: 'Mariachi', label: `Mariachi Band`},
-
-        { value: 'Dancer', label: `Dancer`},
-        { value: 'Singer', label: `Singer`},
-        { value: 'Acrobat', label: `Acrobat`},
-        { value: 'Visual Artist', label: `Visual Artist`},
-
-
-        { value: 'Furniture Rental', label: `Furniture Rental`},
-        { value: 'Boutique', label: `Boutique`},
-        { value: 'Hairdresser', label: `Hairdresser`},
-        { value: 'Henna/Tattoo Artist', label: `Henna/Tattoo Artist`},
-
-        { value: 'Dress/Tux Rental', label: `Boutique`},
-        { value: 'Limo Rental', label: `Hairdresser`},
-        { value: 'Video Game Truck', label: `Video Game Truck`},
-
-        { value: 'Floral', label: `Floral`},
-        { value: 'Event Venue', label: `Event Venue`},
-
-
-
+      { value: 'AV', label: `Audio Visual Equipment (AV)`},
+      { value: 'Bounce House', label: `Bounce House`},
+      { value: 'Boutique', label: `Boutique`},
+      { value: 'Casino Rental', label: `Casino`},
+      { value: 'Circus', label: `Circus / Acrobat`},
+      { value: 'Clown', label: `Clown`},
+      { value: 'Comedian', label: `Comedian`},
+      { value: 'Dancer', label: `Dancer`},
+      { value: 'Decorator', label: `Decorator`},
+      { value: 'DJ', label: 'DJ' },
+      { value: 'Dress / Tux Rental', label: `Dress / Tux Rental`},
+      { value: 'Event Venue', label: `Event Venue`},
+      { value: 'Floral', label: `Floral`},
+      { value: 'Food Catering', label: `Food Catering`},
+      { value: 'Food Truck', label: `Food Truck`},
+      { value: 'Furniture Rental', label: `Furniture Rental`},
+      { value: 'Hairdresser', label: `Hairdresser`},
+      { value: 'Henna / Tattoo Artist', label: `Henna / Tattoo Artist`},
+      { value: 'Limo Rental', label: `Limo Rental`},
+      { value: 'Live Band', label: `Live Band`},
+      { value: 'Magician', label: `Magician`},
+      { value: 'Mariachi', label: `Mariachi Band`},
+      { value: 'Petting Zoo', label: `Petting Zoo`},
+      { value: 'Photography', label: 'Photography'},
+      { value: 'Photo Booth', label: `Photo Booth`},  
+      { value: 'Singer', label: `Singer`},
+      { value: 'Video Game Truck', label: `Video Game Truck`},
+      { value: 'Visual Artist', label: `Visual Artist`}
       ],
     MAX_NUM_SERVICES: 5, 
     MAX_MEDIA_IMAGES: 5
