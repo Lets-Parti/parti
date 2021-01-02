e8ea246ae9284c5216f011704aaac7b7c105e92f
Matthew Wang <matt8p@outlook.com>
2021-01-02 16:29:43 -0700

    icon

--- src/pages/home.js
+++ src/pages/home.js
@@ -19,14 +19,14 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 
 const discoverCategories = [
-  ["Comedian", <EmojiEmotionsIcon />],
-  ["Decorator", <PaletteIcon />],
-  ["DJ", <AlbumIcon />],
-  ["Event Venue", <BusinessIcon />],
-  ["Food Catering", <LocalDiningIcon />],
-  ["Magician", <BrushIcon />],
-  ["Petting Zoo", <PetsIcon />],
-  ["Photography", <PhotoCameraIcon />]
+  ["Comedian", <EmojiEmotionsIcon fontSize="large"/>],
+  ["Decorator", <PaletteIcon fontSize="large"/>],
+  ["DJ", <AlbumIcon fontSize="large"/>],
+  ["Event Venue", <BusinessIcon fontSize="large"/>],
+  ["Food Catering", <LocalDiningIcon fontSize="large"/>],
+  ["Magician", <BrushIcon fontSize="large"/>],
+  ["Petting Zoo", <PetsIcon fontSize="large"/>],
+  ["Photography", <PhotoCameraIcon fontSize="large"/>]
 ];
 
 
