77cd42d48f602890c82a7ed42dabda0dbb765b0c
Matthew Wang <matt8p@outlook.com>
2021-01-24 14:12:48 -0500

    tracker

--- src/pages/home.js
+++ src/pages/home.js
@@ -78,6 +78,10 @@ class Home extends React.Component {
     componentDidMount()
     {
       firebaseAnalytics.logEvent("home_visited");
+      const uniqueID = this.props.match.params.uniqueID; 
+      if(uniqueID){
+         firebaseAnalytics.logEvent(`outreach_clicked_${uniqueID}`);
+      }
     }
 
     eventChange(event)
