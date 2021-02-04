336150e03e3db045c953916264d2de6b343940dd
Matthew Wang <matt8p@outlook.com>
2021-02-04 14:03:47 -0500

    webinar

---
+++ src/pages/webinar.js
@@ -0,0 +1,16 @@
+import React from "react";
+class Webinar extends React.Component {
+    constructor()
+    {   
+        super(); 
+    }   
+    componentDidMount()
+    {
+        window.location.href = "https://www.eventbrite.com/e/meet-parti-tickets-140013174173";
+    }
+    render() {
+        return(null);
+    }
+}
+
+export default Webinar;
